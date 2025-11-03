1f0034cf153b5e51d9f49f85c3b6e166814f589f
dipsyrara <rdeantika8@gmail.com>
2025-11-03 23:23:54 +0700

    first

--- tugas_topo.py
+++ tugas_topo.py
@@ -3,54 +3,67 @@ from mininet.net import Mininet
 from mininet.node import Node
 from mininet.link import TCLink
 from mininet.cli import CLI
-from mininet.log import setLogLevel
-
-class LinuxRouter( Node ):
-    def config( self, **params ):
-        super( LinuxRouter, self ).config( **params )
-        self.cmd( 'sysctl -w net.ipv4.ip_forward=1' )
-
-    def terminate( self ):
-        self.cmd( 'sysctl -w net.ipv4.ip_forward=0' )
-        super( LinuxRouter, self ).terminate()
+from mininet.log import setLogLevel, info
 
 def myNetwork():
     net = Mininet(controller=None, link=TCLink)
 
-    r1 = net.addHost('r1', cls=LinuxRouter)
-    r2 = net.addHost('r2', cls=LinuxRouter)
-    r3 = net.addHost('r3', cls=LinuxRouter)
+    info('*** Adding routers\n')
+    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r2 = net.addHost('r2', cls=Node, ip='0.0.0.0')
+    r3 = net.addHost('r3', cls=Node, ip='0.0.0.0')
 
+    for r in (r1, r2, r3):
+        r.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    info('*** Adding hosts\n')
     h1 = net.addHost('h1', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
     h2 = net.addHost('h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
     h3 = net.addHost('h3', ip='10.0.4.2/24', defaultRoute='via 10.0.4.1')
 
-    # Link dengan konfigurasi IP in-line
-    net.addLink(h1, r1, intfName2='r1-eth0', params2={'ip': '10.0.0.1/24'})
-    net.addLink(h2, r2, intfName2='r2-eth1', params2={'ip': '10.0.2.1/24'})
-    net.addLink(h3, r3, intfName2='r3-eth1', params2={'ip': '10.0.4.1/24'})
+    info('*** Creating links\n')
+    net.addLink(h1, r1)     # 10.0.0.0/24
+    net.addLink(r1, r2)     # 10.0.1.0/24
+    net.addLink(r2, h2)     # 10.0.2.0/24
+    net.addLink(r2, r3)     # 10.0.3.0/24
+    net.addLink(r3, h3)     # 10.0.4.0/24
 
-    net.addLink(r1, r2, intfName1='r1-eth1', params1={'ip': '10.0.1.1/24'},
-                        intfName2='r2-eth0', params2={'ip': '10.0.1.2/24'})
-    
-    net.addLink(r2, r3, intfName1='r2-eth2', params1={'ip': '10.0.3.1/24'},
-                        intfName2='r3-eth0', params2={'ip': '10.0.3.2/24'})
-
+    info('*** Building network\n')
     net.build()
 
-    # Rute statis ditambahkan setelah network di-build
+    info('*** Assigning IPs to router interfaces\n')
+    
+    r1.setIP('10.0.0.1/24', intf='r1-eth0')
+    r1.setIP('10.0.1.1/24', intf='r1-eth1')
+
+    r2.setIP('10.0.1.2/24', intf='r2-eth0')
+    r2.setIP('10.0.2.1/24', intf='r2-eth1')
+    r2.setIP('10.0.3.1/24', intf='r2-eth2')
+
+    # R3
+    r3.setIP('10.0.3.2/24', intf='r3-eth0')
+    r3.setIP('10.0.4.1/24', intf='r3-eth1')
+
+    info('*** Setting up static routes\n')
+
+    # Routes for R1
     r1.cmd('ip route add 10.0.2.0/24 via 10.0.1.2')
     r1.cmd('ip route add 10.0.3.0/24 via 10.0.1.2')
     r1.cmd('ip route add 10.0.4.0/24 via 10.0.1.2')
 
+    # Routes for R2
     r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1')
     r2.cmd('ip route add 10.0.4.0/24 via 10.0.3.2')
 
+    # Routes for R3
     r3.cmd('ip route add 10.0.0.0/24 via 10.0.3.1')
     r3.cmd('ip route add 10.0.1.0/24 via 10.0.3.1')
     r3.cmd('ip route add 10.0.2.0/24 via 10.0.3.1')
 
+    info('*** Running CLI\n')
     CLI(net)
+
+    info('*** Stopping network\n')
     net.stop()
 
 if __name__ == '__main__':
